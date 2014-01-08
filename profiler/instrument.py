50a52198290c33fdc00c502da594bf839e5a12e6
Robert Warner <radicalphoenix@gmail.com>
2014-01-08 11:03:24 -0800

    fixes formatting to be mostly pep8 compliant, ignores line length recommendations

--- profiler/instrument.py
+++ profiler/instrument.py
@@ -3,11 +3,11 @@ from datetime import datetime
 from django.db.models.sql.compiler import SQLCompiler
 from django.db.models.sql.datastructures import EmptyResultSet
 from django.db.models.sql.constants import MULTI
-from django.db import connection
 
 from aggregate.client import get_client
 
 from profiler import _get_current_view
+
 
 def execute_sql(self, *args, **kwargs):
     client = get_client()
@@ -27,11 +27,10 @@ def execute_sql(self, *args, **kwargs):
         return self.__execute_sql(*args, **kwargs)
     finally:
         d = (datetime.now() - start)
-        client.insert({'query' : q, 'view' : _get_current_view(), 'type' : 'sql'},
-                      {'time' : 0.0 + d.seconds * 1000 + d.microseconds/1000, 'count' : 1})
+        client.insert({'query': q, 'view': _get_current_view(), 'type': 'sql'},
+                      {'time': 0.0 + d.seconds * 1000 + d.microseconds/1000, 'count': 1})
 
 INSTRUMENTED = False
-
 
 
 if not INSTRUMENTED:
